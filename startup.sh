6db1c9632ba237e64817b3d12ad29bb6b04e8318
Dalius Dobravolskas <daliusd@wix.com>
2020-12-08 14:30:24 +0200

    OK.

--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Remap tilde key
-hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035},{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064}]}'
