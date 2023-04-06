9b9699d006d01216631094aeb2458982545088c5
Dalius Dobravolskas <daliusd@wix.com>
2023-04-06 16:54:40 +0300

    Neovim: some fun with messages_en.json

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -934,3 +934,32 @@ vim.api.nvim_create_user_command(
   end,
   {}
 )
+
+local function extract_message()
+  local s_buf, s_row, s_col, _ = unpack(vim.fn.getpos("v"))
+  local _, e_row, e_col, _ = unpack(vim.fn.getpos("."))
+
+  local value = vim.api.nvim_buf_get_text(s_buf, s_row - 1, s_col - 1, e_row - 1, e_col, {})[1]
+  value = value:gsub('"', '\\"')
+
+  local key = vim.fn.input("Key: ", "")
+  if #key == 0 then
+    return
+  end
+
+  key = key:gsub('"', '\\"')
+  local tr_key = 't("' .. key .. '")'
+  vim.api.nvim_buf_set_text(s_buf, s_row - 1, s_col - 1, e_row - 1, e_col, { tr_key })
+
+  local out_file = vim.fn.system('fd messages_en.json')
+  out_file = out_file:gsub("[\n]", "")
+  local message_file_lines = vim.fn.readfile(out_file)
+  message_file_lines[#message_file_lines - 1] = message_file_lines[#message_file_lines - 1] .. ','
+  table.insert(message_file_lines, #message_file_lines, '  ' .. '"' .. key .. '": "' .. value .. '"')
+
+  vim.fn.writefile(message_file_lines, out_file)
+
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<esc>', true, false, true), 'x', false)
+end
+
+vim.keymap.set('v', '<leader>z', extract_message, { noremap = true, silent = true })
