2bbdb05b0d1c3d0b85118dd3782956877bfa104e
Dalius Dobravolskas <daliusd@wix.com>
2023-04-19 17:05:48 +0300

    Nvim: sort back

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -288,6 +288,10 @@ require("lazy").setup({
   'editorconfig/editorconfig-vim',
 
   {
+    'sQVe/sort.nvim',
+    config = true,
+  },
+  {
     'windwp/nvim-ts-autotag',
     config = true,
   },
@@ -449,6 +453,10 @@ keymap('n', 'gd', ':tabclose<cr>', opts)
 keymap('n', 'ga', ':tabnew<cr>', opts)
 keymap('n', 'gs', ':tab split<cr>', opts)
 keymap('n', 'go', ':tabonly<cr>', opts)
+
+-- Sort
+keymap('n', 'gh', ':Sort<cr>', opts)
+keymap('v', 'gh', '<esc>:Sort<cr>', opts)
 
 -- The 66-character line (counting both letters and spaces) is widely regarded as ideal.
 -- http://webtypography.net/Rhythm_and_Proportion/Horizontal_Motion/2.1.2/
