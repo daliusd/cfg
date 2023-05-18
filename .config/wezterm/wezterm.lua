376d9cf8289e09a0f737a18c33538efc0d31d27a
Dalius Dobravolskas <daliusd@wix.com>
2023-05-18 09:10:58 +0300

    Maximise wezterm on start.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -129,4 +129,11 @@ config.keys = {
   },
 }
 
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window {}
+  window:gui_window():maximize()
+end)
+
 return config
