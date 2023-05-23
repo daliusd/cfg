b18e6db3c105d46f5a081824c38e1322e6b762c8
Dalius Dobravolskas <daliusd@wix.com>
2023-05-23 09:59:06 +0300

    image-diff: different diffing

--- bin/image-diff.sh
+++ bin/image-diff.sh
@@ -9,20 +9,25 @@ if [[ -z "$GIT_DIFF_IMAGE_ENABLED" ]]; then
 else
   echo "Diffing ${bn}"
   diff="$(mktemp -t "$bn").png"
-  final="$(mktemp -t "$bn").png"
   gm compare -highlight-style assign -highlight-color red -file "$diff" "$1" "$2"
 
-  w=$(gm identify -format "%w" "$2")
-  h=$(gm identify -format "%h" "$2")
-  if ((w > h)); then
-    gm convert "$2" "$1" +append "$final"
-    gm convert "$final" "$diff" -append "$final"
-  else
-    gm convert "$2" "$diff" "$1" +append "$final"
-  fi
-  # resize large diffs
-  gm mogrify -resize 2800x\> "$final"
+  # ALTERNATIVE WAY where we combine images
+  # final="$(mktemp -t "$bn").png"
+  # w=$(gm identify -format "%w" "$2")
+  # h=$(gm identify -format "%h" "$2")
+  # if ((w > h)); then
+  #   gm convert "$2" "$1" +append "$final"
+  #   gm convert "$final" "$diff" -append "$final"
+  # else
+  #   gm convert "$2" "$diff" "$1" +append "$final"
+  # fi
+  # # resize large diffs
+  # gm mogrify -resize 2800x\> "$final"
 
-  img2sixel "$final"
+  img2sixel "$diff"
+  echo "Old:"
+  img2sixel "$2"
+  echo "New:"
+  img2sixel "$1"
 
 fi
