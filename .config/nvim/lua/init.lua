c412b3261dfae65764a06bb57a967e23e04aa407
Dalius Dobravolskas <daliusd@wix.com>
2023-06-07 10:23:22 +0300

    nvim: snippets from auto-complete removed. I am not using them anyway.

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -223,16 +223,6 @@ require("lazy").setup({
     config = function()
       local cmp = require 'cmp'
 
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local feedkey = function(key, mode)
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-      end
-
       cmp.setup({
         snippet = {
           expand = function(args)
@@ -251,25 +241,14 @@ require("lazy").setup({
           ['<C-j>'] = cmp.mapping.select_next_item(),
           ['<C-k>'] = cmp.mapping.select_prev_item(),
           ['<CR>'] = cmp.mapping.confirm({ select = false }),
-          ["<S-Tab>"] = cmp.mapping(function()
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ['<Tab>'] = function(fallback)
             if cmp.visible() then
-              cmp.select_prev_item()
-            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-              feedkey("<Plug>(vsnip-jump-prev)", "")
+              cmp.select_next_item()
+            else
+              fallback()
             end
-          end, { "i", "s" }),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item()
-              elseif vim.fn["vsnip#available"](1) == 1 then
-                feedkey("<Plug>(vsnip-expand-or-jump)", "")
-              elseif has_words_before() then
-                cmp.complete()
-              else
-                fallback()
-              end
-            end,
-            { "i", "s" })
+          end
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp', priority = 20 },
@@ -286,7 +265,6 @@ require("lazy").setup({
             }
           },
           { name = 'path',     priority = 8 },
-          { name = 'vsnip',    priority = 7 },
           { name = 'emoji',    priority = 7 },
           { name = 'git',      priority = 6 },
         }),
