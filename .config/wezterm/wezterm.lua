f34e0694aa38aa8ef74acd99efb4783d9a1cc559
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2023-06-23 22:50:25 +0300

    wezterm: hide windows decorations

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,6 +11,8 @@ config.scrollback_lines = 10000
 config.color_scheme = 'zenbones'
 config.font = wezterm.font { family = 'VictorMono Nerd Font Mono', weight = 'Bold' }
 
+config.window_decorations = "RESIZE"
+
 local f = io.popen("uname")
 local s = f:read("*a")
 s = s:gsub("%s+", "")
