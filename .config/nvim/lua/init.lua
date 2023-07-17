fff9f72d1eccabbec75b2882e6fe5772f9e7bbf7
Dalius Dobravolskas <daliusd@wix.com>
2023-07-17 16:25:46 +0300

    nvim: leap added

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -228,6 +228,17 @@ require("lazy").setup({
     end
   },
   {
+    "ggandor/leap.nvim",
+    config = function()
+      vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward-to)');
+      vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward-to)');
+      require('leap').add_repeat_mappings('<tab>', '<s-tab>', {
+        relative_directions = true,
+        modes = { 'n', 'x', 'o' },
+      })
+    end
+  },
+  {
     "kylechui/nvim-surround",
     config = true,
   },
