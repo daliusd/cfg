d67c9bf9dcf3e0faf6644a4a9539e08bf5204472
Dalius Dobravolskas <daliusd@wix.com>
2023-09-08 13:27:15 +0300

    image-diff for git improved

--- bin/image-diff.sh
+++ bin/image-diff.sh
@@ -8,25 +8,28 @@ if [[ -z "$GIT_DIFF_IMAGE_ENABLED" ]]; then
   echo "Diffing disabled for \"$bn\". Use 'git diff-image' to see image diffs."
 else
   echo "Diffing ${bn}"
-  diff="$(mktemp -t "$bn.XXXXXXX").png"
-  gm compare -highlight-style assign -highlight-color red -file "$diff" "$1" "$2"
+  if [[ "$2" != "/dev/null" ]]; then
+    diff="$(mktemp -t "$bn.XXXXXXX").png"
+    gm compare -highlight-style assign -highlight-color red -file "$diff" "$1" "$2"
 
-  # ALTERNATIVE WAY where we combine images
-  # final="$(mktemp -t "$bn").png"
-  # w=$(gm identify -format "%w" "$2")
-  # h=$(gm identify -format "%h" "$2")
-  # if ((w > h)); then
-  #   gm convert "$2" "$1" +append "$final"
-  #   gm convert "$final" "$diff" -append "$final"
-  # else
-  #   gm convert "$2" "$diff" "$1" +append "$final"
-  # fi
-  # # resize large diffs
-  # gm mogrify -resize 2800x\> "$final"
+    # ALTERNATIVE WAY where we combine images
+    # final="$(mktemp -t "$bn").png"
+    # w=$(gm identify -format "%w" "$2")
+    # h=$(gm identify -format "%h" "$2")
+    # if ((w > h)); then
+    #   gm convert "$2" "$1" +append "$final"
+    #   gm convert "$final" "$diff" -append "$final"
+    # else
+    #   gm convert "$2" "$diff" "$1" +append "$final"
+    # fi
+    # # resize large diffs
+    # gm mogrify -resize 2800x\> "$final"
 
-  img2sixel "$diff"
-  echo "Old:"
-  img2sixel "$2"
+    img2sixel "$diff"
+    echo "Old:"
+    img2sixel "$2"
+  fi
+
   echo "New:"
   img2sixel "$1"
 
