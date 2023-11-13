32aed5bcdd4ecae41a4a08c7b94b65bcb84e04b0
Dalius Dobravolskas <daliusd@wix.com>
2023-11-13 10:20:49 +0200

    nvim: lspkind

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -390,9 +390,11 @@ require("lazy").setup({
       'hrsh7th/cmp-vsnip',
       'hrsh7th/vim-vsnip',
       'hrsh7th/vim-vsnip-integ',
+      'onsails/lspkind.nvim',
     },
     config = function()
       local cmp = require 'cmp'
+      local lspkind = require('lspkind')
 
       cmp.setup({
         snippet = {
@@ -401,8 +403,8 @@ require("lazy").setup({
           end,
         },
         window = {
-          -- completion = cmp.config.window.bordered(),
-          -- documentation = cmp.config.window.bordered(),
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -445,6 +447,14 @@ require("lazy").setup({
           { name = 'path',     priority = 8 },
           { name = 'emoji',    priority = 7 },
         }),
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = 'symbol',       -- show only symbol annotations
+            maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          })
+        }
+
       })
 
       cmp.setup.cmdline('/', {
