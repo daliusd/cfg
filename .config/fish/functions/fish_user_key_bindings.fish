5a75045c020ac1cfa13a440860c2aeb655ef60c0
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2024-02-10 23:18:12 +0200

    fish: ctrl-j / ctrl-k

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -2,5 +2,8 @@ function fish_user_key_bindings
   fish_default_key_bindings -M insert
   fish_vi_key_bindings --no-erase insert
 
+  bind -M insert \ck up-or-search
+  bind -M insert \cj down-or-search
+
   fzf_key_bindings
 end
