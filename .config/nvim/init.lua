222b47aae2e9ad743d666ad564109b5923777d15
Dalius Dobravolskas <daliusd@wix.com>
2024-04-23 10:51:10 +0300

    nvim: playing around with snippets

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -381,6 +381,7 @@ require("lazy").setup({
     end
   },
   'windwp/nvim-autopairs',
+  { "rafamadriz/friendly-snippets" },
   {
     'hrsh7th/nvim-cmp',
     event = "InsertEnter",
@@ -395,6 +396,16 @@ require("lazy").setup({
       'onsails/lspkind.nvim',
     },
     config = function()
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local feedkey = function(key, mode)
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+      end
+
       local cmp = require 'cmp'
       local lspkind = require('lspkind')
 
@@ -416,15 +427,25 @@ require("lazy").setup({
           ['<C-j>'] = cmp.mapping.select_next_item(),
           ['<C-k>'] = cmp.mapping.select_prev_item(),
           ['<CR>'] = cmp.mapping.confirm({ select = false }),
-          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ["<S-Tab>"] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+              feedkey("<Plug>(vsnip-jump-prev)", "")
+            end
+          end, { "i", "s" }),
           ['<Tab>'] = function(fallback)
             if cmp.visible() then
               cmp.select_next_item()
+            elseif vim.fn["vsnip#available"](1) == 1 then
+              feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            elseif has_words_before() then
+              cmp.complete()
             else
               fallback()
             end
           end
-        }),
+        }, { "i", "s" }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp', priority = 20 },
           {
@@ -448,6 +469,7 @@ require("lazy").setup({
           },
           { name = 'path',     priority = 8 },
           { name = 'emoji',    priority = 7 },
+          { name = 'vsnip',    priority = 6 },
         }),
         formatting = {
           format = lspkind.cmp_format({
