114dabe294a8adc39a3d530b00494bf046c5fa21
Dalius Dobravolskas <daliusd@wix.com>
2024-07-13 00:40:15 +0300

    nvim: some fun with oil.nvim, wezterm and image preview

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -81,3 +81,83 @@ vim.api.nvim_create_user_command(
 )
 
 vim.cmd(":cnoreabbrev <expr> rg (getcmdtype() == ':' && getcmdline() ==# 'rg') ? 'Rg' : 'rg'")
+
+local timer
+vim.api.nvim_create_autocmd({ "CursorMoved", "ModeChanged" }, {
+  desc = "Preview in wezterm",
+  callback = function()
+    local oil = require("oil")
+    if vim.wo.previewwindow then
+      return
+    end
+
+    -- Debounce and update the preview window
+    if timer then
+      timer:again()
+      return
+    end
+    timer = vim.loop.new_timer()
+    if not timer then
+      return
+    end
+    timer:start(10, 100, function()
+      timer:stop()
+      timer:close()
+      timer = nil
+      vim.schedule(function()
+        local entry = oil.get_cursor_entry()
+
+        -- Don't update in visual mode. Visual mode implies editing not browsing,
+        -- and updating the preview can cause flicker and stutter.
+        local mode = vim.api.nvim_get_mode().mode
+        local is_visaul_mode = mode:match("^[vV]") ~= nil
+
+        if entry and not is_visual_mode then
+          local full_path = oil.get_current_dir() .. entry.name
+
+          -- find focused pane
+          local listClientsResult = vim.system({'wezterm', 'cli', 'list-clients', '--format=json'}, { text = true}):wait()
+          local focused_pane_id = vim.json.decode(listClientsResult.stdout)[1].focused_pane_id
+
+          local listResult = vim.system({'wezterm', 'cli', 'list', '--format=json'}, { text = true}):wait()
+          local panes = vim.json.decode(listResult.stdout)
+
+          -- find relevant tab
+          local tab_id = 0
+          for _, v in ipairs(panes) do
+            if v.pane_id == focused_pane_id then
+              tab_id = v.tab_id
+              break
+            end
+          end
+
+          -- find pane for preview
+          local preview_pane_id = -1
+          for _, v in ipairs(panes) do
+            if v.tab_id == tab_id and v.pane_id ~= focused_pane_id then
+              preview_pane_id = v.pane_id
+              break
+            end
+          end
+
+          -- create preview pane if we don't have one
+          if preview_pane_id == -1 then
+            local splitPaneResult = vim.system({'wezterm', 'cli', 'split-pane', '--right'}, { text = true}):wait()
+            preview_pane_id = splitPaneResult.stdout
+
+            os.execute('wezterm cli activate-pane --pane-id ' .. focused_pane_id)
+          end
+
+          if full_path:match('.png$') then
+            local command = 'wezterm cli send-text --no-paste --pane-id ' .. preview_pane_id .. ' "viu ' .. full_path .. '\r\n"'
+            os.execute(command)
+          elseif full_path:match('.svg$') then
+            local command = 'wezterm cli send-text --no-paste --pane-id ' .. preview_pane_id .. ' "gm convert ' .. full_path .. ' PNG:- | viu -\r\n"'
+            os.execute(command)
+          end
+        end
+      end)
+    end)
+  end,
+})
+
