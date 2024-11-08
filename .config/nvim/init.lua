cd74ac0ba1f48198d612aca30f39281f38405b1a
Dalius Dobravolskas <daliusd@wix.com>
2024-11-08 17:59:58 +0200

    nvim: minor change

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -327,7 +327,7 @@ require("lazy").setup({
         typescriptreact = { 'langd', 'eslint_d' },
       }
 
-      vim.api.nvim_create_autocmd({ "BufRead", "InsertLeave" }, {
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
         callback = function()
           require("lint").try_lint()
         end,
