3f78a82253d87f0ecace1518a1ef678022096582
Dalius Dobravolskas <daliusd@wix.com>
2024-12-03 14:09:51 +0200

    nvim: leap nvim is not working for me

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -205,17 +205,6 @@ require("lazy").setup({
   --   config = true,
   -- },
   {
-    "ggandor/leap.nvim",
-    config = function()
-      vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward-to)');
-      vim.keymap.set({ 'n' }, 'S', '<Plug>(leap-backward-to)');
-      require('leap').add_repeat_mappings('<tab>', '<s-tab>', {
-        relative_directions = true,
-        modes = { 'n', 'x', 'o' },
-      })
-    end
-  },
-  {
     "kylechui/nvim-surround",
     config = true,
   },
