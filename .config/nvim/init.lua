3caec6293cc7595ae5a3df5648543573c52d8177
Dalius Dobravolskas <daliusd@wix.com>
2024-12-11 09:54:39 +0200

    nvim: blink change

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -416,9 +416,6 @@ require("lazy").setup({
         ['<Tab>'] = { 'select_next', 'fallback' },
       },
 
-      highlight = {
-        use_nvim_cmp_as_default = true,
-      },
       nerd_font_variant = 'mono',
       accept = { auto_brackets = { enabled = true } },
       trigger = { signature_help = { enabled = true } },
