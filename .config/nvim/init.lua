97df0864afc94993dba1f585e709605c60695f6b
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2025-03-16 17:57:20 +0200

    nvim: mini.ai

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -365,9 +365,28 @@ require("lazy").setup({
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
+  { 'nvim-treesitter/nvim-treesitter-textobjects' },
   {
-    "kylechui/nvim-surround",
-    config = true,
+    'echasnovski/mini.ai',
+    version = '*',
+    config = function()
+      local gen_spec = require('mini.ai').gen_spec
+      require('mini.ai').setup({
+        custom_textobjects = {
+          -- Tweak argument to be recognized only inside `()` between `;`
+          a = gen_spec.argument({ brackets = { '%b()' }, separator = ';' }),
+
+          -- Tweak function call to not detect dot in function name
+          f = gen_spec.function_call({ name_pattern = '[%w_]' }),
+
+          -- Function definition (needs treesitter queries with these captures)
+          F = gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }),
+
+          -- Make `|` select both edges in non-balanced way
+          ['|'] = gen_spec.pair('|', '|', { type = 'non-balanced' }),
+        }
+      })
+    end
   },
   {
     'numToStr/Comment.nvim',
