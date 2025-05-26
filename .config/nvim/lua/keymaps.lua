33760b8e46ab9bc92dae73e568d49503f025b54e
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2025-05-26 23:57:06 +0300

    nvim: incremental selection improvement

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -64,13 +64,54 @@ keymap('x', 's', search_with_two_chars('/'), opts)
 keymap('n', 'S', search_with_two_chars('?'), opts)
 keymap('x', 'S', search_with_two_chars('?'), opts)
 
+-- Incremental selection
+_G.selected_nodes = {}
 
-vim.keymap.set({ "n" }, "<cr>", function()
+local function get_node_at_cursor()
   local node = vim.treesitter.get_node()
-  if not node then return end
+  if not node then return nil end
+  return node
+end
 
+local function select_node(node)
+  if not node then return end
   local start_row, start_col, end_row, end_col = node:range()
   vim.fn.setpos("'<", { 0, start_row + 1, start_col + 1, 0 })
   vim.fn.setpos("'>", { 0, end_row + 1, end_col, 0 })
   vim.cmd('normal! gv')
+end
+
+vim.keymap.set({ "n" }, "<tab>", function()
+  _G.selected_nodes = {}
+
+  local current_node = get_node_at_cursor()
+  if not current_node then return end
+
+  table.insert(_G.selected_nodes, current_node)
+  select_node(current_node)
 end, { desc = "Select treesitter node" })
+
+
+vim.keymap.set("v", "<tab>", function()
+  if #_G.selected_nodes == 0 then
+    return
+  end
+
+  local current_node = _G.selected_nodes[#_G.selected_nodes]
+
+  if not current_node then return end
+
+  local parent = current_node:parent()
+  if parent then
+    table.insert(_G.selected_nodes, parent)
+    select_node(parent)
+  end
+end, { desc = "Increment selection" })
+
+vim.keymap.set("v", "<S-tab>", function()
+  table.remove(_G.selected_nodes)
+  local current_node = _G.selected_nodes[#_G.selected_nodes]
+  if current_node then
+    select_node(current_node)
+  end
+end, { desc = "Decrement selection" })
