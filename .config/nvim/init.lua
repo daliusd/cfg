84ec55cec8d8a868e3624debe15bce84f4ab3132
Dalius Dobravolskas <daliusd@wix.com>
2025-08-25 11:36:33 +0300

    nvim: opencode.nvim config update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -921,19 +921,24 @@ require('lazy').setup({
   },
   {
     'NickvanDyke/opencode.nvim',
-    dependencies = { 'folke/snacks.nvim' },
-    ---@type opencode.Config
-    opts = {},
+    dependencies = {
+      -- Recommended for better prompt input, and required to use opencode.nvim's embedded terminal — otherwise optional
+      { 'folke/snacks.nvim', opts = { input = { enabled = true } } },
+    },
+    ---@type opencode.Opts
+    opts = {
+      -- Your configuration, if any — see lua/opencode/config.lua
+    },
     -- stylua: ignore
     keys = {
-      { '<leader>ot', function() require('opencode').toggle() end, desc = 'Toggle embedded opencode', },
-      { '<leader>oa', function() require('opencode').ask() end, desc = 'Ask opencode', mode = 'n', },
+      { '<leader>oA', function() require('opencode').ask() end, desc = 'Ask opencode', },
+      { '<leader>oa', function() require('opencode').ask('@cursor: ') end, desc = 'Ask opencode about this', mode = 'n', },
       { '<leader>oa', function() require('opencode').ask('@selection: ') end, desc = 'Ask opencode about selection', mode = 'v', },
-      { '<leader>op', function() require('opencode').select_prompt() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
       { '<leader>on', function() require('opencode').command('session_new') end, desc = 'New session', },
       { '<leader>oy', function() require('opencode').command('messages_copy') end, desc = 'Copy last message', },
       { '<S-C-u>',    function() require('opencode').command('messages_half_page_up') end, desc = 'Scroll messages up', },
       { '<S-C-d>',    function() require('opencode').command('messages_half_page_down') end, desc = 'Scroll messages down', },
+      { '<leader>op', function() require('opencode').select_prompt() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
     },
   },
 })
