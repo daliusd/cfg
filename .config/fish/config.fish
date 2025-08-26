f7d3d3f52a41f18187da06f75019b36eace9663a
Dalius Dobravolskas <daliusd@wix.com>
2025-08-26 09:24:49 +0300

    fish: `ai` update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -80,7 +80,7 @@ alias h='history --merge'
 
 alias dnsflush='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
 
-alias ai='gh models run gpt-4.1-mini'
+alias ai='gh models run openai/gpt-4.1-mini'
 # FZF
 
 set FZF_DEFAULT_COMMAND 'fd -t f'
