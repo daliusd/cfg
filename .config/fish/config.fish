80e47752c64a75f78eb4c5ced585f968a6af3b2b
Dalius Dobravolskas <daliusd@wix.com>
2025-09-17 13:21:58 +0300

    fish: yolo command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -81,6 +81,8 @@ alias h='history --merge'
 alias dnsflush='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
 
 alias ai='gh models run openai/gpt-4.1-mini'
+
+alias yolo='gh pr create -f; gh pr comment -b "#skipreview"; gh pr merge --auto --squash'
 # FZF
 
 set FZF_DEFAULT_COMMAND 'fd -t f'
