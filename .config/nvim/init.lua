cb073024a3c9db3b6d17e1ad319b87b58a26ff2b
Dalius Dobravolskas <daliusd@wix.com>
2025-09-18 13:32:28 +0300

    nvim: lsp eslint fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -491,15 +491,21 @@ require('lazy').setup({
       vim.lsp.enable('cssls', true)
       vim.lsp.enable('cssmodules_ls', true)
 
+      local base_on_attach = vim.lsp.config.eslint.on_attach
       vim.lsp.config('eslint', {
         settings = {
           packageManager = 'yarn',
         },
         ---@diagnostic disable-next-line: unused-local
         on_attach = function(client, bufnr)
+          if not base_on_attach then
+            return
+          end
+
+          base_on_attach(client, bufnr)
           vim.api.nvim_create_autocmd('BufWritePre', {
             buffer = bufnr,
-            command = 'EslintFixAll',
+            command = 'LspEslintFixAll',
           })
         end,
       })
