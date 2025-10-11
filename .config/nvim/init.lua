a112da7e60cff67776c71a5296bd8b69b27b33de
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2025-10-11 19:42:43 +0300

    nvim: lighter config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -897,29 +897,6 @@ require('lazy').setup({
       file_types = { 'markdown' },
     },
   },
-  {
-    'NickvanDyke/opencode.nvim',
-    dependencies = {
-      -- Recommended for better prompt input, and required to use opencode.nvim's embedded terminal — otherwise optional
-      { 'folke/snacks.nvim', opts = { input = { enabled = true } } },
-    },
-    ---@type opencode.Opts
-    opts = {
-      -- Your configuration, if any — see lua/opencode/config.lua
-    },
-    -- stylua: ignore
-    keys = {
-      { '<leader>oA', function() require('opencode').ask() end, desc = 'Ask opencode', },
-      { '<leader>oa', function() require('opencode').ask('@cursor: ') end, desc = 'Ask opencode about this', mode = 'n', },
-      { '<leader>of', function() require('opencode').ask('@buffer ') end, desc = 'Ask opencode about this buffer', mode = 'n', },
-      { '<leader>oa', function() require('opencode').ask('@selection: ') end, desc = 'Ask opencode about selection', mode = 'v', },
-      { '<leader>on', function() require('opencode').command('session_new') end, desc = 'New session', },
-      { '<leader>oy', function() require('opencode').command('messages_copy') end, desc = 'Copy last message', },
-      { '<S-C-u>',    function() require('opencode').command('messages_half_page_up') end, desc = 'Scroll messages up', },
-      { '<S-C-d>',    function() require('opencode').command('messages_half_page_down') end, desc = 'Scroll messages down', },
-      { '<leader>op', function() require('opencode').select_prompt() end, desc = 'Select prompt', mode = { 'n', 'v', }, },
-    },
-  },
 })
 
 require('misc')
