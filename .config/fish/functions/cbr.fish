978e65dbb585e9c837746dc2d6be67c68bc9ee85
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2025-11-06 20:53:19 +0200

    fish: cbr function improvement

--- .config/fish/functions/cbr.fish
+++ .config/fish/functions/cbr.fish
@@ -1,8 +1,10 @@
 function cbr -d "Checkout remote branch (fetch first)"
   git fetch --all
+  set current_branch (git branch --show-current)
   git branch -r |
     grep --invert-match '\->' |
     sed 's/^[[:space:]]*origin\///' |
+    grep --invert-match "^$current_branch\$" |
     fzf --preview="git log origin/{} --color=always --oneline -n 20" |
     xargs git checkout
 end
