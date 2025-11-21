10699a86248a9c298388e77a7f1dc5f12349f784
Dalius Dobravolskas <daliusd@wix.com>
2025-11-21 17:58:53 +0200

    nodeinstall update

--- bin/nodeinstall.sh
+++ bin/nodeinstall.sh
@@ -3,8 +3,8 @@ npm install -g vscode-langservers-extracted
 npm install -g yaml-language-server
 npm install -g @astrojs/language-server
 npm install -g cssmodules-language-server
-npm install -g @fsouza/prettierd
-npm install -g @daliusd/langd
+npm install -g @daliusd/prettier-lsp
+npm install -g @daliusd/lang-lsp
 npm install -g npm-check-updates
 npm install -g tsx
 npm install -g typescript-language-server
