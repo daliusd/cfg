0901ba25156f3029a093e46c76e250e18180bcde
Dalius Dobravolskas <dalius.dobravolskas@gmail.com>
2025-12-08 18:50:04 +0200

    nvim: leap.nvim: new location

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -259,7 +259,7 @@ require('lazy').setup({
     config = true,
   },
   {
-    'ggandor/leap.nvim',
+    'https://codeberg.org/andyg/leap.nvim',
     config = function()
       vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)')
       vim.keymap.set({ 'n' }, 'S', '<Plug>(leap-backward)')
