4bb0e7e3ab4204b4609850de35b93241bd876eae
Dalius Dobravolskas <daliusd@wix.com>
2025-12-12 12:44:47 +0200

    fish: bunjs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -31,6 +31,7 @@ fish_add_path -P ~/bin
 fish_add_path -P ~/.local/bin
 fish_add_path -P ~/.npm-global/bin
 fish_add_path -P ~/.cargo/bin
+fish_add_path -P ~/.bun/bin
 
 alias ls='lsd'
 alias ll='ls -al'
