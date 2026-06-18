17b774e1ab4888e3a12e61b86734ed8f2d2a8c7c
Dalius Dobravolskas <daliusd@wix.com>
2026-06-18 11:07:17 +0300

    nvim: ghlite.nvim back

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -654,6 +654,34 @@ require('lazy').setup({
   },
   'tpope/vim-abolish',
   'rachartier/tiny-cmdline.nvim',
+  {
+    dir = '~/projects/ghlite.nvim',
+    config = function()
+      require('ghlite').setup({
+        debug = false,
+        view_split = '',
+        diff_split = '',
+        open_command = 'open',
+        keymaps = {
+          diff = {
+            open_file_tab = 'gt',
+          },
+        },
+      })
+    end,
+    keys = {
+      { '<leader>us', ':GHLitePRSelect<cr>', silent = true, desc = 'PR Select' },
+      { '<leader>uo', ':GHLitePRCheckout<cr>', silent = true, desc = 'PR Checkout' },
+      { '<leader>uv', ':GHLitePRView<cr>', silent = true, desc = 'PR View' },
+      { '<leader>uu', ':GHLitePRLoadComments<cr>', silent = true, desc = 'PR Load Comments' },
+      { '<leader>up', ':GHLitePRDiff<cr>', silent = true, desc = 'PR Diff' },
+      { '<leader>ua', ':GHLitePRAddComment<cr>', silent = true, desc = 'PR Add comment' },
+      { '<leader>ua', ':GHLitePRAddComment<cr>', mode = 'x', silent = true, desc = 'PR Add comment' },
+      { '<leader>uc', ':GHLitePRUpdateComment<cr>', silent = true, desc = 'PR Update comment' },
+      { '<leader>ud', ':GHLitePRDeleteComment<cr>', silent = true, desc = 'PR Delete comment' },
+      { '<leader>ug', ':GHLitePROpenComment<cr>', silent = true, desc = 'PR Open comment' },
+    },
+  },
 })
 
 require('misc')
